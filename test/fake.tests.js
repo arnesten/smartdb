24054234e5e0157ae11ff5c87a16c596f239e51e
Calle Arnesten <calle.arnesten@codekick.com>
2014-02-12 11:06:08 +0100

    * Can now force async mode for the fake * Changed version to 1.2.0

--- test/fake.tests.js
+++ test/fake.tests.js
@@ -18,7 +18,29 @@ module.exports = testCase('fake', {
             assert.equals(doc, { _id: 'F1', type: 'fish', name: 'Shark' });
             done();
         });
-    }
+    },
+	'can force async mode': function () {
+		var fish1 = { _id: 'F1', type: 'fish', name: 'Shark' };
+		var fish2 = { _id: 'F2', type: 'fish', name: 'Shark' };
+		var fakeDb = createFake({
+			async: true,
+			entities: [fish1, fish2]
+		});
+
+		var stub = sinon.stub();
+
+		fakeDb.get('fish', 'F1', stub);
+		fakeDb.getOrNull('fish', 'F1', stub);
+		fakeDb.save(fish1, stub);
+		fakeDb.update(fish1, stub);
+		fakeDb.merge('fish', 'F2', {}, stub);
+		fakeDb.remove('fish', 'F2', stub);
+		fakeDb.view('fish', 'inTheSea', { }, stub);
+		fakeDb.viewRaw('fish', 'inTheSea', { }, stub);
+		fakeDb.viewValue('fish', 'inTheSea', { }, stub);
+
+		refute.called(stub);
+	}
 });
 
 function createFake(options) {
