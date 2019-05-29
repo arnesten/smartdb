14f4d57aac574a00d83d04848c492e72a8330960
arnesten <calle.arnesten@codekick.com>
2019-05-29 22:56:33 +0200

    If more than one ID generating conflict happens, keep trying until success

--- test/generate-id.js.tests.js
+++ test/generate-id.js.tests.js
@@ -73,6 +73,39 @@ module.exports = testCase('generate-id', {
             name: 'Shark',
             type: 'fish'
         });
+    },
+    'when conflict happens twice after generating ID on save should try with new ID': async function () {
+        this.nock
+            .put('/main/S1A', { name: 'Shark', type: 'fish' }).reply(409, {})
+            .put('/main/S2A', { name: 'Shark', type: 'fish' }).reply(409, {})
+            .put('/main/S3A', { name: 'Shark', type: 'fish' }).reply(200, { id: 'S3A', rev: 'S3B' });
+        let callCount = 0;
+        let db = createDb({
+            databases: [
+                {
+                    url: 'http://myserver.com/main',
+                    entities: {
+                        fish: {}
+                    }
+                }
+            ],
+            generateId(doc) {
+                callCount++;
+                assert.equals(doc, { name: 'Shark', type: 'fish' });
+                return Promise.resolve(`S${callCount}A`);
+            }
+        });
+
+        let fish = { name: 'Shark', type: 'fish' };
+
+        await db.save(fish);
+
+        assert.equals(fish, {
+            _id: 'S3A',
+            _rev: 'S3B',
+            name: 'Shark',
+            type: 'fish'
+        });
     }
 });
 
