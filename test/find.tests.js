6cad7135f97836b0469d6a7ed1c91bcb44185f65
arnesten <calle.arnesten@codekick.com>
2019-07-11 08:57:12 +0200

    wip

--- test/find.tests.js
+++ test/find.tests.js
@@ -71,8 +71,62 @@ module.exports = testCase('find', {
             }
         });
 
-        assert.equals(result.length, 1);
-        assert.equals(result[0], { _id: 'F1', name: 'Great white' });
+        assert.equals(result, [{ _id: 'F1', name: 'Great white' }]);
+    },
+    'can use a hook to add information': async function () {
+        this.nock
+            .post('/animals/_find', {
+                selector: {
+                    name: 'Great white',
+                    use_index: 'byName'
+                },
+                foo: 'bar'
+            })
+            .reply(200, {
+                docs: [
+                    { _id: 'F1', name: 'Great white' }
+                ]
+            });
+        let db = createDb({
+            databases: [{
+                url: 'http://myserver.com/animals',
+                entities: {
+                    fish: {}
+                }
+            }],
+            findHook: args => {
+                args.foo = 'bar';
+            }
+        });
+
+        let result = await db.find('fish', 'byName', {
+            selector: {
+                name: 'Great white'
+            }
+        });
+
+        assert.equals(result, [{ _id: 'F1', name: 'Great white' }]);
+    },
+    'can use a hook to validate': async function () {
+        let db = createDb({
+            databases: [{
+                url: 'http://myserver.com/animals',
+                entities: {
+                    fish: {}
+                }
+            }],
+            findHook: () => {
+                throw new Error('Invalid');
+            }
+        });
+
+        let err = await catchError(() => db.find('fish', 'byName', {
+            selector: {
+                name: 'Great white'
+            }
+        }));
+
+        assert.equals(err.message, 'Invalid');
     }
 });
 
@@ -82,4 +136,13 @@ function createDb(options) {
 
 function Fish(doc) {
     Object.assign(this, doc);
+}
+
+async function catchError(fn) {
+    try {
+        await fn();
+    }
+    catch (err) {
+        return err;
+    }
 }
